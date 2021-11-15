d8331cea9ea5cd83d1278205f7fb5153bcfccf6b
2santosalan <alan.santosmolina@gmail.com>
2021-11-15 00:15:18 -0600

    primer coment

---
+++ resources/views/layouts/footers/auth.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+  <div class="container-fluid">
+    {{-- <nav class="float-left">
+      <ul>
+        <li>
+          <a href="http://indeporte.cdmx.gob.mx/"">
+              {{ __('Indeporte') }}
+          </a>
+        </li>
+        <li>
+          <a href="https://creative-tim.com/presentation">
+              {{ __('About Us') }}
+          </a>
+        </li>
+        <li>
+          <a href="http://blog.creative-tim.com">
+              {{ __('Blog') }}
+          </a>
+        </li>
+        <li>
+          <a href="https://www.creative-tim.com/license">
+              {{ __('Licenses') }}
+          </a>
+        </li>
+      </ul>
+    </nav> --}}
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>{{-- , made with --}} {{--< i class="material-icons">favorite</i>  by--}}
+      <a href="http://indeporte.cdmx.gob.mx/" target="_blank">Indeporte</a>
+    </div>
+  </div>
+</footer>
